026bba3cb33a001eafff1cdd57d1abb4fde12c02
Carlos Lee <charlee593@gmail.com>
2020-07-01 17:54:06 -0700

    Remove unnecessary dependency.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 """Packaging settings."""
+import os
+
 from setuptools import setup
 from okdatasetgen import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name = 'oktopus_datasetgen',
@@ -15,9 +20,8 @@ setup(
     long_description_content_type='text/markdown',
     url = 'https://github.com/Oktopus-Multicast/oktopus_dataset-gen.git',
     packages=['okdatasetgen'],
+    install_requires=required,
     classifiers = [
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: MIT License'
     ],
     entry_points = {
